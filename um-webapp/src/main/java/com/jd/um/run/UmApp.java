5abf4a68964b4dc7acd98a79798cbb9ddf2f6f15
lmarinve <lmarin@hotmail.com>
2016-09-02 15:22:20 -0400

    All

--- um-webapp/src/main/java/com/jd/um/run/UmApp.java
+++ um-webapp/src/main/java/com/jd/um/run/UmApp.java
@@ -1,43 +1,62 @@
 package com.jd.um.run;
 
-import com.jd.um.persistence.setup.MyApplicationContextInitializer;
 import com.jd.um.spring.UmContextConfig;
 import com.jd.um.spring.UmPersistenceJpaConfig;
 import com.jd.um.spring.UmServiceConfig;
-import com.jd.um.spring.UmServletConfig;
 import com.jd.um.spring.UmWebConfig;
-import org.springframework.boot.SpringApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication(exclude = { // @formatter:off
         ErrorMvcAutoConfiguration.class
 })// @formatter:on
+@Import({ // @formatter:off
+    UmContextConfig.class,
+    UmPersistenceJpaConfig.class,
+    UmServiceConfig.class,
+    UmWebConfig.class
+}) // @formatter:on
 public class UmApp extends SpringBootServletInitializer {
 
-    private final static Object[] CONFIGS = { // @formatter:off
-            UmContextConfig.class,
-            UmPersistenceJpaConfig.class,
-            UmServiceConfig.class,
-            UmWebConfig.class,
-            UmServletConfig.class,
 
-            UmApp.class
-    }; // @formatter:on
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean dispatcherServletRegistration() {
+        final ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet(), "/*");
+
+        final Map<String, String> params = new HashMap<String, String>();
+        params.put("contextClass", "org.springframework.web.context.support.AnnotationConfigWebApplicationContext");
+        params.put("contextConfigLocation", "org.spring.sec2.spring");
+        params.put("dispatchOptionsRequest", "true");
+        registration.setInitParameters(params);
+
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
 
     //
-
+	//
     @Override
-    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
-        return application.sources(CONFIGS).initializers(new MyApplicationContextInitializer());
-    }
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {        
+    	return application.sources(UmApp.class);    }
 
     public static void main(final String... args) {
-        final SpringApplication springApplication = new SpringApplication(CONFIGS);
-        springApplication.addInitializers(new MyApplicationContextInitializer());
-        springApplication.run(args);
+    	new SpringApplicationBuilder(UmApp.class).listeners().run(args);    }
     }
 
-}
+
+
